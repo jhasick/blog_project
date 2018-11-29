feabb74637d6d4c194069e041af15a3ce7280667
Jeff Hasick <jhasick@nc.rr.com>
2018-11-29 08:12:30 -0500

    updated blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_142401) do
+ActiveRecord::Schema.define(version: 2018_11_28_151115) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "comment_entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
